b3fd38d6da1de6c164fc230eeb21777384ee0300
Daniel Madar <daniel.madar@snyk.io>
2022-02-03 16:34:38 +0200

    - moved the description to readme

--- quotes/quotes/spiders/quotes.py
+++ quotes/quotes/spiders/quotes.py
@@ -1,22 +1,6 @@
 
 import scrapy
 
-"""
-This Spider is scrapping 'quotes' site.
-
-Each quote element has:
-1. The quote
-2. The author
-3. Tags for the quote
-4. Link with further details about the author
-
-For each item - 
-We iterate over it and scrap the item fields and also the 'more info' details behind the link.
-We yield a dictionary in json with all the relevant details
-
-After we complete to scrap the page - we search for a 'Next' button and continue the process for the next page. 
-"""
-
 
 class QuoteSpider(scrapy.Spider):
 
